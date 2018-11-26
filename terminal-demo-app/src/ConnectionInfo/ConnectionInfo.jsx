aed1f13d488b96a77cc70eaf4642f95bae4b37ba
Fay Wu <fay@stripe.com>
2018-11-26 14:16:01 -0800

    Add Connection Info section

--- terminal-demo-app/src/ConnectionInfo/ConnectionInfo.jsx
+++ terminal-demo-app/src/ConnectionInfo/ConnectionInfo.jsx
@@ -3,6 +3,26 @@ import * as React from "react";
 import { breakpoints } from "../styles.jsx";
 import { css } from "emotion";
 import Group from "../lib/Group/Group.jsx";
+import OverflowMenu from "../lib/OverflowMenu/OverflowMenu.jsx";
+import Text from "../lib/Text/Text.jsx";
+
+const commonCSS = `
+    background: #ffffff;
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0);
+    height: 44px;
+    flex-shrink: 0;
+    padding: 16px 20px;
+    ${breakpoints.laptop} {
+    width: 310px;
+    }
+    ${breakpoints.mobile} {
+    width: 100%;
+    }
+    border-bottom: 1px solid #e3e8ee;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
 
 class ConnectionInfo extends React.Component {
   render() {
@@ -10,40 +30,21 @@ class ConnectionInfo extends React.Component {
       <Group direction="column" spacing={0}>
         <div
           className={css`
-            background: #ffffff;
+            ${commonCSS}
             border-radius: 14px 14px 0 0;
-            box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0);
-            height: 44px;
-            flex-shrink: 0;
-            padding: 16px 20px;
-            ${breakpoints.laptop} {
-              width: 310px;
-            }
-            ${breakpoints.mobile} {
-              width: 100%;
-            }
-            border-bottom: 1px solid #e3e8ee;
           `}
         >
-          hello
+          <Text color="lightGrey">Set API key</Text>
+          <OverflowMenu />
         </div>
         <div
           className={css`
-            background: #ffffff;
+            ${commonCSS}
             border-radius: 0 0 14px 14px;
-            box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0);
-            height: 44px;
-            flex-shrink: 0;
-            padding: 16px 20px;
-            ${breakpoints.laptop} {
-              width: 310px;
-            }
-            ${breakpoints.mobile} {
-              width: 100%;
-            }
           `}
         >
-          hello
+          <Text color="lightGrey">Connect to a reader</Text>
+          <OverflowMenu />
         </div>
       </Group>
     );
---
+++ terminal-demo-app/src/lib/OverflowMenu/OverflowMenu.jsx
@@ -0,0 +1,102 @@
+import * as React from "react";
+
+import { css } from "emotion";
+
+class OverflowMenu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shown: true
+    };
+  }
+
+  componentWillMount() {
+    document.addEventListener("mousedown", this.onClick);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.onClick);
+  }
+
+  onClick = e => {
+    if (this.node.contains(e.target)) {
+      this.setState({ shown: !this.state.shown });
+    } else {
+      this.setState({ shown: false });
+    }
+  };
+
+  render() {
+    const { shown } = this.state;
+
+    return (
+      <div
+        className={css`
+          display: flex;
+        `}
+        ref={node => (this.node = node)}
+      >
+        <button
+          className={css`
+            height: 20px;
+            width: 20px;
+            border: none;
+
+            background-image: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48dGl0bGU+c3RhdGljPC90aXRsZT48cGF0aCBkPSJNNSAxMS43NWExLjc1IDEuNzUgMCAxIDEgMC0zLjUgMS43NSAxLjc1IDAgMCAxIDAgMy41em01IDBhMS43NSAxLjc1IDAgMSAxIDAtMy41IDEuNzUgMS43NSAwIDAgMSAwIDMuNXptNSAwYTEuNzUgMS43NSAwIDEgMSAwLTMuNSAxLjc1IDEuNzUgMCAwIDEgMCAzLjV6IiBmaWxsPSIjODg5OEFBIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiLz48L3N2Zz4=");
+            cursor: pointer;
+
+            transition: background-image 0.08s ease-in;
+
+            :hover {
+              background-image: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48dGl0bGU+dGFyZ2V0SG92ZXI8L3RpdGxlPjxnIGZpbGw9IiM0MjQ3NzAiIGZpbGwtcnVsZT0iZXZlbm9kZCI+PGNpcmNsZSBmaWxsLW9wYWNpdHk9Ii4xIiBjeD0iMTAiIGN5PSIxMCIgcj0iMTAiLz48cGF0aCBkPSJNNSAxMS43NWExLjc1IDEuNzUgMCAxIDEgMC0zLjUgMS43NSAxLjc1IDAgMCAxIDAgMy41em01IDBhMS43NSAxLjc1IDAgMSAxIDAtMy41IDEuNzUgMS43NSAwIDAgMSAwIDMuNXptNSAwYTEuNzUgMS43NSAwIDEgMSAwLTMuNSAxLjc1IDEuNzUgMCAwIDEgMCAzLjV6Ii8+PC9nPjwvc3ZnPg==");
+            }
+
+            :active {
+              background-image: url("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48dGl0bGU+dGFyZ2V0QWN0aXZlPC90aXRsZT48ZyBmaWxsPSIjMzIzMjVEIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPjxjaXJjbGUgZmlsbC1vcGFjaXR5PSIuMSIgY3g9IjEwIiBjeT0iMTAiIHI9IjEwIi8+PHBhdGggZD0iTTUgMTEuNzVhMS43NSAxLjc1IDAgMSAxIDAtMy41IDEuNzUgMS43NSAwIDAgMSAwIDMuNXptNSAwYTEuNzUgMS43NSAwIDEgMSAwLTMuNSAxLjc1IDEuNzUgMCAwIDEgMCAzLjV6bTUgMGExLjc1IDEuNzUgMCAxIDEgMC0zLjUgMS43NSAxLjc1IDAgMCAxIDAgMy41eiIvPjwvZz48L3N2Zz4=");
+            }
+          `}
+        />
+        {shown && (
+          <div
+            className={css`
+              display: flex;
+              flex-direction: column;
+              position: absolute;
+              transform-origin: 50% -11px;
+              transform: translatey(30px);
+            `}
+          >
+            <div
+              className={css`
+                background: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zd…A0LjQ4LTEuODA3IDYuMTk2IDBMMjAgOS4wOTNIMWw2LjQwMi02Ljc0eiIvPjwvZz48L3N2Zz4=)
+                  no-repeat;
+                height: 21px;
+                width: 21px;
+                background-position: center center;
+                z-index: 1;
+              `}
+            />
+            <div
+              className={css`
+                position: absolute;
+                z-index: 300;
+                background-color: white;
+                box-shadow: 0px 0px 0px 1px rgba(136, 152, 170, 0.1),
+                  0px 15px 35px 0px rgba(49, 49, 93, 0.1),
+                  0px 5px 15px 0px rgba(0, 0, 0, 0.08);
+                border-radius: 4px;
+                padding-bottom: 8px;
+                padding-top: 8px;
+              `}
+            >
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default OverflowMenu;
