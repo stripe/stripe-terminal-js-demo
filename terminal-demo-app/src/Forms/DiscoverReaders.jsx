a63ca96a63b904db045d1267762e0b0dfd676ddb
Fay Wu <fay@stripe.com>
2018-11-27 14:12:09 -0800

    Style button

--- terminal-demo-app/src/Forms/DiscoverReaders.jsx
+++ terminal-demo-app/src/Forms/DiscoverReaders.jsx
@@ -1,10 +1,28 @@
 // @flow
 
 import * as React from "react";
+import Button from "../components/Button/Button.jsx";
+import Group from "../components/Group/Group.jsx";
+import Section from "../components/Section/Section.jsx";
+import Text from "../components/Text/Text.jsx";
 
 class DiscoverReaders extends React.Component {
   render() {
-    return null;
+    return (
+      <Group direction="column" spacing={0}>
+        <Section position="first">Readers</Section>
+        <Section position="last">
+          <Button color="white">
+            <Text color="dark">Register new reader</Text>
+          </Button>
+          <Button color="white">
+            <Text color="dark" size={14}>
+              Use simulator
+            </Text>
+          </Button>
+        </Section>
+      </Group>
+    );
   }
 }
 
--- terminal-demo-app/src/components/Button/Button.jsx
+++ terminal-demo-app/src/components/Button/Button.jsx
@@ -3,21 +3,38 @@ import * as React from "react";
 import { css } from "emotion";
 
 class Button extends React.Component {
+  getColor = color => {
+    switch (color) {
+      case "dark":
+        return "#3B415E";
+      case "grey":
+        return "#C1C9D2";
+      case "lightGrey":
+        return "#8792A2";
+      case "link":
+        return "#78ACF8";
+      case "white":
+        return "#FFFFFF";
+      default:
+        return "#586ADA";
+    }
+  };
+
   render() {
-    const { children, disabled, onClick } = this.props;
+    const { children, color, disabled, onClick } = this.props;
 
     return (
       <button
         className={css`
-          background: #586ada;
+          background: ${this.getColor(color)};
           border-radius: 4px 4px 4px 4px;
-          box-shadow: 0 2px 5px 0 rgba(50, 50, 93, 0),
-            0 1px 1.5px 0 rgba(0, 0, 0, 0);
-          height: 28px;
-          width: 72px;
+          box-shadow: 0 0 0 1px rgba(43, 45, 80, 0.1),
+            0 2px 5px 0 rgba(43, 45, 80, 0.08),
+            0 1px 1.5px 0 rgba(0, 0, 0, 0.07), 0 1px 2px 0 rgba(0, 0, 0, 0.08);
           display: flex;
           align-items: center;
           justify-content: center;
+          padding: 8px;
 
           :disabled {
             pointer-events: none;
