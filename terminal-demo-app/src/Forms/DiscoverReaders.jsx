31386599dfb8528101ecdfe9a39b629733e010b7
Fay Wu <fay@stripe.com>
2018-11-27 11:52:12 -0800

    Add placeholder files

---
+++ terminal-demo-app/src/Forms/DiscoverReaders.jsx
@@ -0,0 +1,11 @@
+// @flow
+
+import * as React from "react";
+
+class DiscoverReaders extends React.Component {
+  render() {
+    return null;
+  }
+}
+
+export default DiscoverReaders;
---
+++ terminal-demo-app/src/Forms/RegisterNewReader.jsx
@@ -0,0 +1,11 @@
+//@flow
+
+import * as React from "react";
+
+class RegisterNewReader extends React.Component {
+  render() {
+    return null;
+  }
+}
+
+export default RegisterNewReader;
