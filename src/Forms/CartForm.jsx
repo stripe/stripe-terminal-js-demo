72328fea23cad7e4474a52d713511463790ac6a6
Leo <33919642+leogoldman@users.noreply.github.com>
2022-02-16 16:53:41 -0500

    Added ability to choose currencies AUD, NZD, SGD (#111)
    * Added ability to choose currencies AUD, NZD, SGD
    
    * Sorted currencies alphabetically

--- src/Forms/CartForm.jsx
+++ src/Forms/CartForm.jsx
@@ -11,10 +11,13 @@ import Select from "../components/Select/Select.jsx";
 
 class CartForm extends React.Component {
   static CURRENCIES = [
-    { value: "usd", label: "USD" },
-    { value: "gbp", label: "GBP" },
+    { value: "aud", label: "AUD" },
     { value: "cad", label: "CAD" },
-    { value: "eur", label: "EUR" }
+    { value: "eur", label: "EUR" },
+    { value: "gbp", label: "GBP" },
+    { value: "nzd", label: "NZD" },
+    { value: "sgd", label: "SGD" },
+    { value: "usd", label: "USD" }
   ];
 
   render() {
